98c5b8d2b718c6a7cdfac09babf30e8ecd8ebc81
ChoiHongYeon <cgo0168@naver.com>
2024-04-02 14:43:02 +0900

    second commit

--- 20240401-1.py
+++ 20240401-1.py
@@ -1,20 +1,13 @@
 # 달리기 경주(https://school.programmers.co.kr/learn/courses/30/lessons/178871)
 
 def solution(players, callings):
-    answer = []
     players_dict={player:i for i,player in enumerate(players)}
-    players_number=list(players_dict.values())
-    callings_number=[players_dict[calling] for calling in callings]
-    
-    for x in range(len(callings_number)):
-        for y in range(1,len(players_number)):
-            if callings_number[x]==players_number[y]:
-                players_number[y-1],players_number[y]=players_number[y],players_number[y-1]
+    players_num=list(range(len(players)))
+    callings_num=[players_dict[calling] for calling in callings]
 
-    for i in range(len(players_number)):
-        answer.append(players[players_number[i]])
-    
+    for x in range(len(callings_num)):
+        y=players_num.index(callings_num[x])
+        players_num[y],players_num[y-1]=players_num[y-1],players_num[y]
+
+    answer=[players[j] for j in players_num]
     return answer
-
-A=solution(["mumu", "soe", "poe", "kai", "mine"],["kai", "kai", "mine", "mine"])
-print(A)
