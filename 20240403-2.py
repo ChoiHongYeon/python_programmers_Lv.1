369af631043533cb17ea8eae98a4f4f3a6ce0236
ChoiHongYeon <cgo0168@naver.com>
2024-04-03 15:05:44 +0900

    second commit

--- 20240403-2.py
+++ 20240403-2.py
@@ -2,23 +2,16 @@
 
 def solution(n, m, section):
     answer = 0
-    A=[]
+    A=[] #[0,0,0,0,0,0,0,0,0,0]
     
-    for a in range(len(section)):
+    for a in range(n+m):
         A.append(0)
 
     for b in range(len(section)):
-        if A[b]==0:
-            A[b]+=1
+        if A[section[b]]==0:
+            A[section[b]]+=1
             answer+=1
-            for c in range(b+1,len(section)):
-                if section[c]<=section[b]+m-1:
-                    A[c]+=1
-    
-    return answer
+            for c in range(1,m):
+                A[section[b]+c]+=1
 
-B=solution(10,3,[1,3,5,6,8])
-C=solution(8,4,[2,3,6])
-D=solution(5,4,[1,3])
-E=solution(4,1,[1,2,3,4])
-print(B,C,D,E)
+    return answer
