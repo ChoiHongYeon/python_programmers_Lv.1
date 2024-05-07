a87a1b9834965eaccbdead51dc6a6f904aee5cb4
ChoiHongYeon <cgo0168@naver.com>
2024-05-08 00:57:17 +0900

    second commit

--- 20240507-1.py
+++ 20240507-1.py
@@ -4,17 +4,11 @@ def solution(number, limit, power):
     answer = 0
     l=[0]*(number+1)
     for i in range(1,number+1):
-        for j in range(1,i+1):
-            if l[i]>limit: break
-            if i%j==0:
-                l[i]+=1
-    for k in range(len(l)):
+        for j in range(0,number+1,i):
+            l[j]+=1
+    for k in range(1,len(l)):
         if l[k]>limit:
             answer+=power
         else:
             answer+=l[k]
     return answer
-
-A=solution(5,3,2)
-B=solution(10,3,2)
-print(A,B)
