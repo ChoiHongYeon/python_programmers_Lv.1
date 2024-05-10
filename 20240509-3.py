ca2d3f0ddb77ca37658cda0ec5de074484a1bfca
ChoiHongYeon <cgo0168@naver.com>
2024-05-10 17:44:50 +0900

    fifth commit

--- 20240509-3.py
+++ 20240509-3.py
@@ -4,12 +4,10 @@ def solution(H):
     answer = 0
     for i in range(len(H)):
         if H[i] == 1:
-            if i + 3 <= len(H) and H[i + 1] == 2 and H[i + 2] == 3 and H[i + 3] == 1:
+            if i+3 <= len(H) and H[i+1] == 2 and H[i+2] == 3 and H[i+3] == 1:
                 answer += 1
-                for j in range(4):
-                    H.pop(i)
-                for j in range(4):
-                    H.insert(0,0)
+                del H[i:i+4]
+                H[0:0] = [0,0,0,0]
     return answer
    
 A = solution([2,1,1,2,3,1,2,3,1])
