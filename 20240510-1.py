9d47f0a5d531d71a2a1e09c2a46db11303a56b25
ChoiHongYeon <cgo0168@naver.com>
2024-05-10 22:57:00 +0900

    second commit

--- 20240510-1.py
+++ 20240510-1.py
@@ -1,19 +1,15 @@
 # 옹알이 (2) (https://school.programmers.co.kr/learn/courses/30/lessons/133499?language=python3)
 
 def solution(babbling):
+    answer = 0
     b = ["aya", "ye", "woo", "ma"]
     bb = ["ayaaya","yeye","woowoo","mama"]
 
-    for i in range(len(b)):
-        for j in range(len(babbling)):
-            if bb[i] not in babbling[j]:
-                babbling[j] = babbling[j].replace(b[i],"")
-
-    answer = babbling.count("")
+    for i in range(len(babbling)):
+        for j in range(len(b)):
+            if bb[j] not in babbling[i]:
+                babbling[i] = babbling[i].replace(b[j],"B")
+        if babbling[i].count("B") == len(babbling[i]):
+            answer += 1
 
     return answer
-
-A=solution(["aya", "yee", "u", "maa"])
-B=solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"])
-
-print(A,B)
