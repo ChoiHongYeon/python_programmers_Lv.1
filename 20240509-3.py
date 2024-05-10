0667807b9f9b2aecd37c3fef968c49fd16d9ebd5
ChoiHongYeon <cgo0168@naver.com>
2024-05-10 17:37:23 +0900

    fourth commit

--- 20240509-3.py
+++ 20240509-3.py
@@ -2,29 +2,14 @@
 
 def solution(H):
     answer = 0
-    h = len(H)
-    for i in range(h):
-        if i >= 0 and i + 3 <= len(H) and H[i] == 1 and H[i + 1] == 2 and H[i + 2] == 3 and H[i + 3] == 1:
-            answer += 1
-            for j in range(4):
-                H.pop(i)
-            H.insert(0,0)
-        if i >= 1 and i + 2 <= len(H) and H[i - 1] == 1 and H[i] == 2 and H[i + 1] == 3 and H[i + 2] == 1:
-            answer += 1
-            for j in range(4):
-                H.pop(i - 1)
-            H.insert(0,0)
-        if i >= 2 and i + 1 <= len(H) and H[i - 2] == 1 and H[i - 1] == 2 and H[i] == 3 and H[i + 1] == 1:
-            answer += 1
-            for j in range(4):
-                H.pop(i - 2)
-            H.insert(0,0)
-        if i >= 3 and i <= len(H) and H[i - 3] == 1 and H[i - 2] == 2 and H[i - 1] == 3 and H[i] == 1:
-            answer += 1
-            for j in range(4):
-                H.pop(i - 3)
-            H.insert(0,0)
-   
+    for i in range(len(H)):
+        if H[i] == 1:
+            if i + 3 <= len(H) and H[i + 1] == 2 and H[i + 2] == 3 and H[i + 3] == 1:
+                answer += 1
+                for j in range(4):
+                    H.pop(i)
+                for j in range(4):
+                    H.insert(0,0)
     return answer
    
 A = solution([2,1,1,2,3,1,2,3,1])
